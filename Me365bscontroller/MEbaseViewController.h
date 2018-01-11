079a41ab6b66165bdb3d3267889de505bbc0df87
张文勇 <>
2018-01-11 18:24:23 +0800

    更新controller的基类 和更新NVcontroller的基类

---
+++ Me365bscontroller/MEbaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  MEbaseViewController.h
+//  Baseviewcontroller
+//
+//  Created by 张文勇 on 2018/1/11.
+//  Copyright © 2018年 张文勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEbaseViewController : UIViewController
+
+@end
